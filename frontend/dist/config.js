e1d6322a8cb4440329728857513fb810aea40ba2
zyrina-is-good <zyrina1608@gmail.com>
2026-06-10 22:47:07 +0700

    Add files via upload

---
+++ frontend/dist/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  // PENTING: Karena kamu menggunakan Github Pages (HTTPS), 
+  // API_URL ini WAJIB menggunakan HTTPS juga!
+  // Jika Panel Jhonale kamu tidak ada HTTPS-nya, ini akan kena NetworkError (Mixed Content).
+  API_URL: "http://djdjjdjd.jhonalehost.web.id:2704"
+};
