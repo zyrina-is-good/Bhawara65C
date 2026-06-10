7abd24cfb751650428dcf02b61d298348ab9695e
zyrina-is-good <zyrina1608@gmail.com>
2026-06-10 22:20:43 +0700

    Update config.js

--- frontend/dist/config.js
+++ frontend/dist/config.js
@@ -1,5 +1,3 @@
 window.APP_CONFIG = {
-  // Ganti URL ini dengan URL VPS DuckDNS kamu
-  // Jangan beri tanda garis miring (/) di akhir URL
-  API_URL: "http://domain-kamu.duckdns.org"
+  API_URL: "http://djdjjdjd.jhonalehost.web.id:25010"
 };
